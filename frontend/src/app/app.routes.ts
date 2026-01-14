0dbaa7ed6b1c7fd43e6ef6394b133d83d2cf0630
Gllbob <gllbob@hotmail.com>
2026-01-13 21:54:27 -0300

    Arquivos Iniciais do Projeto Portifolio

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './view/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: Home
+    },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home' }
+];
